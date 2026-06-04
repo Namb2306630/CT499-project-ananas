f07d8a49acb6799f0c4aa0a2419f6453ab3132c5
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-04 23:45:51 +0700

    thêm validate cho update

--- backend/app/validations/system-config.validation.js
+++ backend/app/validations/system-config.validation.js
@@ -2,22 +2,22 @@ const Joi = require("joi");
 
 const percent = Joi.number().min(0).max(100).required();
 
-exports.updateSystemSchema = Joi.object({
-  vatPercent: percent.messages({
+exports.createSystemSchema = Joi.object({
+  vatPercent: Joi.number().min(0).max(100).required().messages({
     "number.base": "VAT phải là số",
     "number.min": "VAT không được nhỏ hơn 0",
     "number.max": "VAT không được lớn hơn 100",
     "any.required": "VAT là bắt buộc",
   }),
 
-  operatingCostPercent: percent.messages({
+  operatingCostPercent: Joi.number().min(0).max(100).required().messages({
     "number.base": "Chi phí vận hành phải là số",
     "number.min": "Chi phí không được nhỏ hơn 0",
     "number.max": "Chi phí không được lớn hơn 100",
     "any.required": "Chi phí là bắt buộc",
   }),
 
-  profitPercent: percent.messages({
+  profitPercent: Joi.number().min(0).max(100).required().messages({
     "number.base": "Lợi nhuận phải là số",
     "number.min": "Không được < 0",
     "number.max": "Không được > 100",
@@ -30,3 +30,28 @@ exports.updateSystemSchema = Joi.object({
     "any.required": "Ngưỡng free ship là bắt buộc",
   }),
 });
+
+exports.updateSystemSchema = Joi.object({
+  vatPercent: Joi.number().min(0).max(100).messages({
+    "number.base": "VAT phải là số",
+    "number.min": "VAT không được nhỏ hơn 0",
+    "number.max": "VAT không được lớn hơn 100",
+  }),
+
+  operatingCostPercent: Joi.number().min(0).max(100).messages({
+    "number.base": "Chi phí vận hành phải là số",
+    "number.min": "Chi phí không được nhỏ hơn 0",
+    "number.max": "Chi phí không được lớn hơn 100",
+  }),
+
+  profitPercent: Joi.number().min(0).max(100).messages({
+    "number.base": "Lợi nhuận phải là số",
+    "number.min": "Không được < 0",
+    "number.max": "Không được > 100",
+  }),
+
+  freeShippingThreshold: Joi.number().min(0).messages({
+    "number.base": "Ngưỡng free ship phải là số",
+    "number.min": "Không được nhỏ hơn 0",
+  }),
+}).min(1);
