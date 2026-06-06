ac5a41e67368f623ba7898b2fd4e562e7224d86c
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-06 22:59:28 +0700

    thêm middleware cho upload ảnh

--- backend/app/middlewares/upload.middleware.js
+++ backend/app/middlewares/upload.middleware.js
@@ -2,8 +2,14 @@ const { uploadImage } = require("../utils/uploadImage.util");
 
 const uploadBrandLogo = uploadImage("brands").single("logo");
 const uploadCategoryImage = uploadImage("categories").single("image");
+const uploadProductVariantImage = uploadImage("product-variant").fields([
+  { name: "mainImage", maxCount: 1 },
+  { name: "hoverImage", maxCount: 1 },
+  { name: "images", maxCount: 20 },
+]);
 
 module.exports = {
   uploadBrandLogo,
   uploadCategoryImage,
+  uploadProductVariantImage,
 };
