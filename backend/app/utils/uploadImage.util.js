7a74a1a90c2dbac815f52b8e9e7be7fc697aefb6
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-06 22:58:09 +0700

    thay đổi code để lưu tên file ảnh là tên cụ thể

--- backend/app/utils/uploadImage.util.js
+++ backend/app/utils/uploadImage.util.js
@@ -4,8 +4,8 @@ const path = require("path");
 const config = require("../config/index");
 const ErrorCode = require("../constants/errors");
 const AppError = require("../constants/app-error");
-
-// ====== 1. STORAGE CONFIG ======
+// ====== 1. STORAGE CONFIG ======/
+//destination xác định thư mục lưu file
 const storage = (folder) =>
   multer.diskStorage({
     destination: function (req, file, cb) {
@@ -19,13 +19,22 @@ const storage = (folder) =>
       }
 
       cb(null, dir); //lưu file cb là callback, cb(error, destination)
+      //hông lỗi (null) và lưu vào thư dir
       //dir = "uploads/brands";
     },
 
     filename: function (req, file, cb) {
-      const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      // const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      // cb(null, uniqueName + path.extname(file.originalname));
 
-      cb(null, uniqueName + path.extname(file.originalname));
+      const ext = path.extname(file.originalname);
+      const originalName = path.basename(file.originalname, ext);
+      const safeName = originalName
+        .replace(/\s+/g, "_")
+        .replace(/[^\w\-]/g, "");
+
+      cb(null, `${safeName}_${Date.now()}${ext}`);
+
       //1780326848831-427389156.png
     },
     //uploads/brands
@@ -68,14 +77,26 @@ const deleteImage = (filePath) => {
   }
 };
 
-module.exports = {
-  uploadImage,
-  deleteImage,
-};
-
 // {
 //   fieldname: 'logo',
 //   originalname: 'ananas.png',
 //   filename: '1780326848831-427389156.png',
 //   path: 'uploads\\brands\\1780326848831-427389156.png'
 // }
+
+// xóa ảnh khi ko lưu thành công vì multer luôn lưu file trước khi kiểm tra điều kiện
+const removeUploadedFiles = (files) => {
+  if (!files) return;
+
+  Object.values(files)
+    .flat()
+    .forEach((file) => {
+      deleteImage(file.path);
+    });
+};
+
+module.exports = {
+  uploadImage,
+  deleteImage,
+  removeUploadedFiles,
+};
