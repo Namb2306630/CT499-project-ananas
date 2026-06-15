f41a560528d640223e7f70e95eb2c5a800c9d309
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-15 22:39:23 +0700

    comment code

--- backend/app/models/shipping-config.model.js
+++ backend/app/models/shipping-config.model.js
@@ -1,42 +1,42 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const shippingSchema = new mongoose.Schema(
-  {
-    provider: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    // Đơn vị vận chuyển
+// const shippingSchema = new mongoose.Schema(
+//   {
+//     provider: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     // Đơn vị vận chuyển
 
-    province: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    // Khu vực
+//     province: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     // Khu vực
 
-    fee: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    // Phí ship
+//     fee: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//     // Phí ship
 
-    estimatedDays: {
-      type: Number,
-      required: true,
-      min: 1,
-    },
-    // Thời gian giao
+//     estimatedDays: {
+//       type: Number,
+//       required: true,
+//       min: 1,
+//     },
+//     // Thời gian giao
 
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
-    // trạng thái
-  },
-  { timestamps: true },
-);
+//     isActive: {
+//       type: Boolean,
+//       default: true,
+//     },
+//     // trạng thái
+//   },
+//   { timestamps: true },
+// );
 
-module.exports = mongoose.model("ShippingConfig", shippingSchema);
+// module.exports = mongoose.model("ShippingConfig", shippingSchema);
