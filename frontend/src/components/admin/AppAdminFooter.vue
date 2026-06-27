62dd400a3e5219f1152807965ba7b1b4b6eb7acb
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-27 14:16:32 +0700

    footer trang admin

---
+++ frontend/src/components/admin/AppAdminFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <div class="text-center">
+      <p class="copyright m-0 py-2">Copyright &copy; 2026 Ananas. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<style>
+.copyright {
+  color: var(--text-gray);
+}
+footer {
+  background-color: var(--bg-footer-color);
+  border-top: 1px solid var(--border-gray);
+}
+</style>
