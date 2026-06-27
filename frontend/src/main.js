6acbcde39ef37dfcbd25103faeb90ef86db6ffac
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-27 13:51:12 +0700

    thêm cấu hình cho main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
-import './assets/main.css'
-
+import './assets/css/variables.css'
+import './assets/css/global.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'material-symbols'
 
 const app = createApp(App)
 
