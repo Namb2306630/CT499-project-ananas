eaa0fa8bdcac8d52b04f34136c59de3df6ea3e5a
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-27 14:03:00 +0700

    trang layout người dùng

---
+++ frontend/src/layouts/UserLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppFooter from '@/components/user/AppUserFooter.vue'
+import AppHeader from '@/components/user/AppUserHeader.vue'
+// import AppSidebar from '@/components/user/AppSidebar.vue'
+</script>
+
+<template>
+  <div class="app-layout">
+    <AppHeader />
+
+    <main class="content">
+      <RouterView />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
