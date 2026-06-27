b908036319c3db01dcb8dc3d47a0b81246a136fb
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-27 14:08:58 +0700

    tạo demo các trang

---
+++ frontend/src/views/NotFoundPage.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+import NotFound from '@/assets/images/page_not_found.png'
+
+const router = useRouter()
+const route = useRoute() //xem đang ở trang nào
+
+const goHome = () => {
+  if (route.path.startsWith('/admin')) {
+    router.push('/admin/dashboard')
+  } else {
+    router.push('/')
+  }
+}
+</script>
+
+<template>
+  <div class="not-found">
+    <img :src="NotFound" alt="Ảnh Not Found" />
+
+    <button class="btn" @click="goHome">QUAY LẠI TRANG CHỦ</button>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.not-found img {
+  width: 600px;
+  max-width: 90%;
+}
+
+.btn {
+  margin-top: 50px;
+  padding: 22px 65px;
+  border: none;
+  border-radius: 8px;
+  background-color: var(--color-bg-orange);
+  font-weight: var(--font-width-lg);
+  color: var(--text-white);
+  font-size: var(--font-size-lg);
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.btn:hover {
+  transform: translateY(-10px);
+  color: var(--text-white);
+  box-shadow: var(--shadow-black);
+}
+</style>
