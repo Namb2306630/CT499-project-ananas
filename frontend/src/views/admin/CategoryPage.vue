458c6d091f29c7900b3cafe668ef9f66e4fc31ea
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-29 09:13:40 +0700

    Update CategoryPage.vue

--- frontend/src/views/admin/CategoryPage.vue
+++ frontend/src/views/admin/CategoryPage.vue
@@ -1,18 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 import AppAdminPageHeader from '@/components/admin/AppAdminPageHeader.vue'
 import AdminToolbar from '@/components/admin/AdminToolbar.vue'
 import DialogForm from '@/components/admin/forms/DialogForm.vue'
+import { useCategoryStore } from '@/stores/caterory'
+import { storeToRefs } from 'pinia' // giúp giữ tính reactive - tự cập nhật giao diện khi lấy dl từ pinia
+import { useToastStore } from '@/stores/toast'
 
-const categories = ref([])
+const toastStore = useToastStore()
+const categoryStore = useCategoryStore()
+const { categories, loading, error } = storeToRefs(categoryStore)
 const showForm = ref(false)
+
+const loadData = async () => {
+  await categoryStore.fetchCategories()
+}
+
+loadData()
+
+defineProps({
+  showSidebar: Boolean,
+})
 
 const openAddForm = () => {
   showForm.value = !showForm.value
 }
 
-const fields = [
+const fields = computed(() => [
   {
     name: 'name',
     label: 'Tên danh mục *',
@@ -26,10 +41,27 @@ const fields = [
     type: 'select',
     options: categories.value,
   },
-]
+])
 
-const addCategory = (data) => {
-  console.log(data)
+const addCategory = async (data) => {
+  const result = await categoryStore.createCategories(data)
+  if (result) {
+    showForm.value = false
+
+    toastStore.showToast('Thêm danh mục thành công', 'success')
+
+    await loadData()
+  } else {
+    const message =
+      categoryStore.error.general ||
+      Object.values(categoryStore.error.errors)[0] ||
+      'Thêm danh mục thất bại!'
+
+    toastStore.showToast(message, 'error')
+  }
+}
+const clearError = () => {
+  categoryStore.error = {}
 }
 </script>
 
@@ -51,7 +83,37 @@ const addCategory = (data) => {
     <AdminToolbar
       content="Thêm danh mục"
       :items="categories"
+      :loading="loading"
+      :error="error"
       :showAddCard="true"
+      :showSidebar="showSidebar"
+      :fields="[
+        {
+          name: 'image',
+          type: 'image',
+        },
+
+        {
+          name: 'name',
+          type: 'title',
+        },
+        {
+          name: 'productCount',
+          type: 'count',
+        },
+        {
+          name: 'parent',
+          type: 'ref',
+        },
+        {
+          name: 'description',
+          type: 'text',
+        },
+        {
+          name: 'isActive',
+          type: 'status',
+        },
+      ]"
       @add="openAddForm"
     />
   </div>
@@ -64,5 +126,8 @@ const addCategory = (data) => {
     @close="showForm = false"
     title-img="Ảnh danh mục (nếu có)"
     content-img="Thêm ảnh danh mục"
+    :errors="error.errors"
+    :general-error="error.general"
+    @clearError="clearError"
   />
 </template>
