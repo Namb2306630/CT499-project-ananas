1521b30d246eb162acdd1add354a7cecd6ae8c2b
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-29 09:14:06 +0700

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
-<script>
-export default {}
+<script setup>
+import ToastMessage from '@/components/common/ToastMessage.vue'
+console.log(ToastMessage)
 </script>
 
 <template>
+  <ToastMessage />
   <RouterView />
 </template>
-
 <style scoped>
 .page {
   max-width: 400px;
