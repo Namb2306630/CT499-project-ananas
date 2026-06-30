a106d5fddab0b52bdcd36bb3e15feb8b1d6c51c6
Namb2306630 <namhuynh0914923314@gmail.com>
2026-06-30 16:43:44 +0700

    chuyển file vào thư mục khác

---
+++ frontend/src/views/admin/product/ProductVariantPage.vue
@@ -0,0 +1,40 @@
+<script setup>
+import AdminToolbar from '@/components/admin/AdminToolbar.vue'
+import AppAdminPageHeader from '@/components/admin/AppAdminPageHeader.vue'
+import ProductVariantForm from '@/components/admin/forms/ProductVariantForm.vue'
+import { ref } from 'vue'
+
+const products = ref([])
+const showForm = ref(false)
+const errors = ref([])
+
+
+const openAddForm = () => {
+  showForm.value = !showForm.value
+}
+
+const addProduct = (data) => {
+  console.log(data)
+}
+</script>
+
+<template>
+  <div class="admin-container">
+    <AppAdminPageHeader
+      title="Quản Lý Biến Thể Sản Phẩm"
+      description="Quản lý và sắp xếp cấu trúc biến thể sản phẩm của bạn"
+      button-text="THÊM SẢN PHẨM"
+      place-holder="Tìm tên sản phẩm"
+      @click="openAddForm"
+    />
+    <AdminToolbar content="Thêm sản phẩm" @add="openAddForm" :items="products" />
+  </div>
+  <ProductVariantForm
+    :show="showForm"
+    @submit="addProduct"
+    :errors="errors"
+    @close="showForm = false"
+  />
+</template>
+
+<style scoped></style>
