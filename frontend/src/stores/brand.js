08f1b805c228771b98966d4af45fd809f67fe960
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-08 01:43:27 +0700

    tạo pinia quản lý brand

---
+++ frontend/src/stores/brand.js
@@ -0,0 +1,185 @@
+import { defineStore } from 'pinia'
+import BrandService from '@/services/brand.service'
+
+export const useBrandStore = defineStore('brand', {
+  state: () => ({
+    brands: [],
+    brand: null,
+    productLines: [],
+    loading: false,
+    error: {
+      code: null,
+      general: '',
+      errors: {},
+    },
+  }),
+
+  actions: {
+    clearError() {
+      this.error = {
+        code: null,
+        general: '',
+        errors: {},
+      }
+    },
+    async createBrand(form) {
+      try {
+        this.clearError()
+        //một đối tượng trong JavaScript dùng để đóng gói dữ liệu theo định dạng multipart/form-data, thường dùng khi gửi form có file (ảnh, video, PDF...) lên server.
+        const formData = new FormData()
+        formData.append('name', form.name)
+
+        formData.append('logo', form.image)
+
+        formData.append('description', form.description)
+
+        const res = await BrandService.create(formData)
+
+        this.brands.push(res.data.result)
+
+        return {
+          success: true,
+          message: res.data.message,
+        }
+      } catch (error) {
+        const data = error.response?.data
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi tạo dữ liêu thương hiệu!',
+          errors: data?.errors || {},
+        }
+      }
+    },
+
+    async updateBrand(id, form) {
+      try {
+        this.clearError()
+
+        const formData = new FormData()
+
+        formData.append('name', form.name)
+        formData.append('slug', form.slug)
+        formData.append('isActive', form.isActive)
+
+        if (form.image) {
+          formData.append('logo', form.image)
+        }
+
+        const res = await BrandService.update(id, formData)
+
+        const index = this.brands.findIndex((item) => item._id === id)
+
+        if (index !== -1) {
+          this.brands[index] = res.data.result
+        }
+        return {
+          success: true,
+          message: res.data.message,
+        }
+      } catch (error) {
+        const data = error.response?.data
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi cập nhật dữ liệu cho thương hiệu!',
+          errors: data?.errors || {},
+        }
+      }
+    },
+    async fetchAdminBrands() {
+      try {
+        this.loading = true
+        this.clearError()
+        const res = await BrandService.getAll()
+        this.brands = res.data.result.map((brand) => ({
+          ...brand,
+          productLines: brand.productLines?.length || 0,
+          productLineNames: brand.productLines?.map((line) => line.name).join(', '),
+        }))
+      } catch (error) {
+        const data = error.response?.data
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi lấy danh sách thương hiệu!',
+          errors: data?.errors || {},
+        }
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async fetchBrandsForUser() {
+      try {
+        this.loading = true
+        this.clearError()
+        const res = await BrandService.getAllForUser()
+        this.brands = res.data.result
+      } catch (error) {
+        const data = error.response?.data
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi lấy danh sách thương hiệu!',
+          errors: data?.errors || {},
+        }
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async getById(id) {
+      try {
+        this.clearError()
+        const res = await BrandService.getById(id)
+
+        this.brand = res.data.result
+
+        return this.brand
+      } catch (error) {
+        const data = error.response?.data
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi lấy thương hiệu!',
+          errors: data?.errors || {},
+        }
+      }
+    },
+
+    async delete(id) {
+      try {
+        this.clearError()
+        const res = await BrandService.delete(id)
+        this.brands = this.brands.filter((item) => item._id != id)
+
+        return res.data.message
+      } catch (error) {
+        const data = error.response?.data
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi lấy xóa thương hiệu!',
+          errors: data?.errors || {},
+        }
+      }
+    },
+    async fetchProductLines(id) {
+      try {
+        this.loading = true
+        this.clearError()
+
+        const res = await BrandService.getLines(id)
+
+        this.productLines = res.data.result
+
+        return this.productLines
+      } catch (error) {
+        const data = error.response?.data
+
+        this.error = {
+          code: data?.code || 500,
+          general: data?.message || 'Lỗi lấy danh sách dòng sản phẩm!',
+          errors: data?.errors || {},
+        }
+      } finally {
+        this.loading = false
+      }
+    },
+  },
+})
