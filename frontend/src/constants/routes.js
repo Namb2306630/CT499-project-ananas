eb3935e5e35b580fb1596c0713f9d02592bfb398
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-08 01:40:43 +0700

    thêm các API

---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,10 @@
+export const ROUTE_NAMES = {
+  // Admin
+  DASHBOARD: 'dashboard',
+
+  CATEGORY: 'category',
+  CATEGORY_DETAIL: 'category.detail',
+
+  BRAND: 'brand',
+  BRAND_DETAIL: 'brand.detail',
+}
