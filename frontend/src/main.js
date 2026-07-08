74073bf40498d50d1cc4cfeacf672a4280012e55
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-08 17:14:01 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,19 @@
 import './assets/css/variables.css'
 import './assets/css/global.css'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia' //quản lý state
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'material-symbols'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' //Tự động lưu trữ và phục hồi dữ liệu của State (Pinia)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
