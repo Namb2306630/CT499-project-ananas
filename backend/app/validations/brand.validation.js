7fcfa64d4aa4938e77591392db7f89fa5fc54c80
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-08 17:14:33 +0700

    Update brand.validation.js

--- backend/app/validations/brand.validation.js
+++ backend/app/validations/brand.validation.js
@@ -12,8 +12,8 @@ exports.updateBrandSchema = Joi.object({
   name: Joi.string().messages({
     "string.empty": "Tên thương hiệu không được để trống",
   }),
-
+  slug: Joi.string().optional(),
   description: Joi.string().allow(""),
-
   isActive: Joi.boolean(),
+  logo: Joi.any(),
 }).min(0);
