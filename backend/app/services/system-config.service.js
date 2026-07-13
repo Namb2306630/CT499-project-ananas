396c14eecc3cc7784aede8e4d4cb5f8fc0b875b3
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-13 20:51:59 +0700

    thêm upload ảnh cho system

--- backend/app/services/system-config.service.js
+++ backend/app/services/system-config.service.js
@@ -1,5 +1,7 @@
 const SystemConfig = require("../models/system-config.model");
 const ErrorCode = require("../constants/errors");
+const updateFields = require("../utils/updateFields.util");
+const { deleteImage } = require("../utils/uploadImage.util");
 
 class SystemConfigService {
   async get() {
@@ -8,10 +10,43 @@ class SystemConfigService {
 
   async update(payload) {
     const system = await SystemConfig.findOne();
+
     if (!system) throw ErrorCode.SYSTEM_NOT_EXISTS();
-    Object.assign(system, payload);
+
+    updateFields(system, payload, [
+      "email",
+      "hotline",
+      "taxCode",
+      "vatRate",
+      "operatingCostPercent",
+      "profitPercent",
+      "freeShippingThreshold",
+      "currency",
+      "taxDisplayStrategy",
+    ]);
+
     await system.save();
+
+    return system;
+  }
+
+  async updateNotFoundImage(file) {
+    const system = await SystemConfig.findOne();
+
+    if (!system) throw ErrorCode.SYSTEM_NOT_EXISTS();
+
+    // xóa ảnh cũ
+    if (system.notFoundImage) {
+      deleteImage(system.notFoundImage);
+    }
+
+    // lưu đường dẫn giống Brand
+    system.notFoundImage = file.path.replace(/\\/g, "/");
+
+    await system.save();
+
     return system;
   }
 }
+
 module.exports = new SystemConfigService();
