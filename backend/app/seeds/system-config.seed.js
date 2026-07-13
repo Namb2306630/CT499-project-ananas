32106a71cbcc44aeb1eab589cab1e6554c25f9c4
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-13 20:52:02 +0700

    Update system-config.seed.js

--- backend/app/seeds/system-config.seed.js
+++ backend/app/seeds/system-config.seed.js
@@ -3,14 +3,31 @@ const SystemConfig = require("../models/system-config.model");
 const systemConfigSeed = async () => {
   const exists = await SystemConfig.findOne();
 
-  if (exists) return;
+  if (exists) {
+    return;
+  }
 
   await SystemConfig.create({
+    taxCode: "B00000000000000",
+
+    email: "support@ananas.vn",
+    hotline: "1900 1234",
+
     vatPercent: 1.5,
+    vatRate: 1,
+    personalIncomeTaxRate: 0.5,
+
     operatingCostPercent: 15,
     profitPercent: 20,
+
     freeShippingThreshold: 500000,
+
+    currency: "VND",
+    taxDisplayStrategy: "included",
+
+    notFoundImage: "/upload/system/page_not_found.png",
   });
+
 };
 
 module.exports = systemConfigSeed;
