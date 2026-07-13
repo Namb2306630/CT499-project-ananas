12be08902f69346c04f19f41730c7c46985f0f39
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-13 20:52:11 +0700

    Update system-config.controller.js

--- backend/app/controllers/system-config.controller.js
+++ backend/app/controllers/system-config.controller.js
@@ -4,7 +4,7 @@ const ApiResponse = require("../constants/api-response");
 exports.get = async (req, res, next) => {
   try {
     const data = await systemConfigService.get();
-
+    console.log(data.notFoundImage);
     return ApiResponse.success({
       res,
       data,
@@ -20,7 +20,20 @@ exports.update = async (req, res, next) => {
     return ApiResponse.success({
       res,
       data,
-      message: "Cập nhật cấu hình hệ thống thành công",
+      message: "Cập nhật dữ liệu cho cấu hình hệ thống thành công!",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.updateNotFoundImage = async (req, res, next) => {
+  try {
+    const data = await systemConfigService.updateNotFoundImage(req.file);
+
+    return ApiResponse.success({
+      res,
+      data,
+      message: "Cập nhật ảnh 404 thành công",
     });
   } catch (err) {
     next(err);
