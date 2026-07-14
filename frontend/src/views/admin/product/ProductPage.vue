4b3e70aedc7220555cfd2c7824bae1732853f1d3
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-14 22:52:56 +0700

    Update ProductPage.vue

--- frontend/src/views/admin/product/ProductPage.vue
+++ frontend/src/views/admin/product/ProductPage.vue
@@ -2,67 +2,159 @@
 import AdminToolbar from '@/components/admin/AdminToolbar.vue'
 import AppAdminPageHeader from '@/components/admin/AppAdminPageHeader.vue'
 import DialogForm from '@/components/admin/forms/DialogForm.vue'
-import { ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
+import { useToastStore } from '@/stores/toast'
+import { useCategoryStore } from '@/stores/caterory'
+import { useProductLineStore } from '@/stores/product-line'
+import { useProductStore } from '@/stores/product'
+import { useCollectionStore } from '@/stores/collection'
+import { useStyleStore } from '@/stores/style'
+import { storeToRefs } from 'pinia'
+import { useDelete } from '@/composables/useDelete'
+import ConfirmDialog from '@/components/common/ConfirmDialog.vue'
 
-const products = ref([])
+const toastStore = useToastStore()
+const categoryStore = useCategoryStore()
+const productLineStore = useProductLineStore()
+const collectionStore = useCollectionStore()
+const productStore = useProductStore()
+const styleStore = useStyleStore()
+
+const { error: errorCategory, loading: loadingCategory, categories } = storeToRefs(categoryStore)
+const { error: errorProduct, loading: loadingProduct, products } = storeToRefs(productStore)
+const {
+  error: errorCollection,
+  loading: loadingCollection,
+  collections,
+} = storeToRefs(collectionStore)
+const {
+  error: errorProductLine,
+  loading: loadingProductLine,
+  productLines,
+} = storeToRefs(productLineStore)
+
+const { error: errorStyle, loading: loadingStyle, styles } = storeToRefs(styleStore)
+
+const { showConfirm, deleteItem, openDelete, closeDelete } = useDelete()
+
 const showForm = ref(false)
-const errors = ref([])
-const productLines = ref([])
-const productCollections = ref([])
-const categories = ref([])
-const fields = [
+
+onMounted(async () => {
+  await Promise.all([
+    categoryStore.fetchCategories(),
+    productLineStore.fetchProductLines(),
+    collectionStore.fetchForAdmin(),
+    styleStore.fetchForAdmin(),
+    productStore.fetchForAdmin(),
+  ])
+
+  console.log('categories:', categories.value)
+  console.log('productLines:', productLines.value)
+  console.log('collections:', collections.value)
+  console.log('styles:', styles.value)
+  console.log('products:', products.value)
+})
+
+const openAddForm = () => {
+  showForm.value = true
+  productStore.clearError()
+}
+const clearError = () => {
+  productStore.clearError()
+}
+
+const confirmDelete = async () => {
+  const res = await productStore.delete(deleteItem.value._id)
+
+  if (res?.code === 200) {
+    toastStore.showToast(res.message, 'success')
+    closeDelete()
+  } else {
+    const message =
+      Object.values(productStore.error.errors)[0] ||
+      productStore.error.general ||
+      'Xóa sản phẩm thất bại!'
+
+    toastStore.showToast(message, 'error')
+  }
+
+  closeDelete()
+}
+const addProduct = async (data) => {
+  const result = await productStore.create(data)
+
+  if (result?.code === 200) {
+    toastStore.showToast(result.message, 'success')
+  } else {
+    const message =
+      Object.values(productStore.error.errors)[0] ||
+      productStore.error.general ||
+      'Lỗi thêm dữ liệu sản phẩm!'
+
+    toastStore.showToast(message, 'error')
+  }
+}
+
+const fields = computed(() => [
   {
     name: 'name',
     type: 'text',
     label: 'Tên sản phẩm',
     placeholder: 'Nhập tên sản phẩm',
   },
+
   {
     name: 'productLine',
     type: 'select',
     label: 'Dòng sản phẩm',
     placeholder: 'Chọn dòng sản phẩm',
-    options: productLines,
+    options: productLines.value,
   },
+
   {
-    name: 'productCollection',
+    name: 'collection',
     type: 'select',
     label: 'Bộ sưu tập',
     placeholder: 'Chọn bộ sưu tập',
-    options: productCollections,
+    options: collections.value,
   },
 
   {
     name: 'categories',
-    type: 'select',
+    type: 'multiselect',
     label: 'Danh mục',
     placeholder: 'Chọn danh mục',
-    options: categories,
+    options: categories.value,
   },
+
   {
     name: 'costPrice',
     type: 'text',
     label: 'Giá nhập',
     placeholder: 'Giá nhập sản phẩm',
   },
+
   {
     name: 'gender',
-    type: 'checkbox',
+    type: 'radio',
     label: 'Sản phẩm dành cho',
     placeholder: 'Chọn loại sản phẩm',
     options: ['Nam', 'Nữ', 'Phi giới tính'],
   },
+
   {
     name: 'discountPercent',
     type: 'text',
     label: 'Giảm giá',
     placeholder: 'Nhập phần trăm giảm giá',
   },
+
   {
     name: 'styles',
     type: 'select',
     label: 'Kiểu dáng',
     placeholder: 'Chọn kiểu dáng sản phẩm',
+    options: styles.value,
   },
 
   {
@@ -78,15 +170,7 @@ const fields = [
     label: 'Mô tả sản phẩm',
     placeholder: 'Mô tả sản phẩm',
   },
-]
-
-const openAddForm = () => {
-  showForm.value = !showForm.value
-}
-
-const addProduct = (data) => {
-  console.log(data)
-}
+])
 </script>
 
 <template>
@@ -98,14 +182,29 @@ const addProduct = (data) => {
       place-holder="Tìm tên sản phẩm"
       @click="openAddForm"
     />
-    <AdminToolbar content="Thêm sản phẩm" @add="openAddForm" :items="products" />
+    <AdminToolbar
+      content="Thêm sản phẩm"
+      @add="openAddForm"
+      :items="products"
+      @delete="openDelete"
+    />
   </div>
   <DialogForm
     title="Thêm sản phẩm"
     :fields="fields"
     :show="showForm"
     @submit="addProduct"
-    :errors="errors"
+    :errors="errorProduct"
     @close="showForm = false"
+    @clear-error="clearError"
+  />
+
+  <ConfirmDialog
+    :show="showConfirm"
+    title="Xóa sản phẩm"
+    message="Bạn có chắc chắn muốn xóa sản phẩm này?"
+    :name="deleteItem?.name"
+    @confirm="confirmDelete"
+    @cancel="showConfirm = false"
   />
 </template>
