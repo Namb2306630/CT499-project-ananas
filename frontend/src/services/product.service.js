b1de1b2ce6ba098a7f403d6a59dfe95cd73985d8
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-14 22:52:36 +0700

    Update product.service.js

--- frontend/src/services/product.service.js
+++ frontend/src/services/product.service.js
@@ -1,5 +1,22 @@
 import api from '@/api/axios'
 
 export default {
-    
+  create(data) {
+    return api.post('/admin/products', data)
+  },
+  update(id, data) {
+    return api.put(`/admin/products/${id}`, data)
+  },
+  delete(id) {
+    return api.delete(`/admin/products/${id}`)
+  },
+  fetchForAdmin() {
+    return api.get('/admin/products')
+  },
+  fetchForUser() {
+    return api.get('/products')
+  },
+  getBySlug(slug) {
+    return api.get(`/admin/products/${slug}`)
+  },
 }
