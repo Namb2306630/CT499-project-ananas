a34177f53fa379f5359f9e84ed6c513e00c68897
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-18 01:11:43 +0700

    Update DetailLayout.vue

--- frontend/src/components/admin/detail/DetailLayout.vue
+++ frontend/src/components/admin/detail/DetailLayout.vue
@@ -4,12 +4,29 @@ defineProps({
     type: String,
     default: 'Thông tin cơ bản',
   },
+  icon: {
+    type: String,
+    default: 'info',
+  },
+
+  iconType: {
+    type: String,
+    default: 'material', // material | fa
+  },
 })
 </script>
 
 <template>
   <div class="basic-info-card">
     <div class="card-header">
+      <!-- Material -->
+      <span v-if="iconType === 'material'" class="material-symbols-outlined">
+        {{ icon }}
+      </span>
+
+      <!-- FontAwesome -->
+      <i v-else-if="iconType === 'fa'" :class="icon" class="iconI"></i>
+
       <h3>{{ title }}</h3>
     </div>
 
@@ -30,6 +47,24 @@ defineProps({
 .card-header {
   padding: 18px 30px;
   border-bottom: 1px solid var(--border-gray-2);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+}
+
+.card-header span {
+  color: var(--color-bule);
+  font-size: 25px;
+  /* border-left: 4px solid var(--color-bule);
+  padding-left: 12px; */
+}
+
+.card-header .fa-solid {
+  font-size: 20px;
+  padding-left: 12px;
+  color: rgb(230, 230, 20);
+  margin-right: 13px;
 }
 
 .card-header h3 {
@@ -37,11 +72,16 @@ defineProps({
   font-size: 24px;
   font-weight: 700;
   color: var(--text-gray-4);
-  border-left: 4px solid var(--color-bule);
-  padding-left: 12px;
 }
 
 .info-card-1 {
   padding: 10px 30px 30px 30px;
+}
+
+.iconI {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: start;
 }
 </style>
