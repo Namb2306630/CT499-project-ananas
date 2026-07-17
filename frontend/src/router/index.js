f9b3cc5ba9aaf33af330b04700aa368261634151
Namb2306630 <namhuynh0914923314@gmail.com>
2026-07-17 12:03:35 +0700

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,19 +8,19 @@ import { ROUTE_NAMES } from '@/constants/routes.js'
 
 //admin
 import AdminLayout from '@/layouts/AdminLayout.vue'
-import DashBoardPage from '@/views/admin/DashBoardPage.vue'
-import CategoryPage from '@/views/admin/category/CategoryPage.vue'
-import CollectionPage from '@/views/admin/collection/CollectionPage.vue'
-import SystemConfigPage from '@/views/admin/system/SystemConfigPage.vue'
-import StylePage from '@/views/admin/style/StylePage.vue'
-import OrderPage from '@/views/order/OrderPage.vue'
-import ProductLinePage from '@/views/admin/product/ProductLinePage.vue'
-import ProductPage from '@/views/admin/product/ProductPage.vue'
-import UserPage from '@/views/admin/user/UserPage.vue'
-import BrandPage from '@/views/admin/brand/BrandPage.vue'
-import ProductVariantPage from '@/views/admin/product/ProductVariantPage.vue'
-import SystemNotFoundPage from '@/views/admin/system/NotFoundPage.vue'
-import ProductTypePage from '@/views/admin/product-type/ProductTypePage.vue'
+// import DashBoardPage from '@/views/admin/DashBoardPage.vue'
+// import CategoryPage from '@/views/admin/category/CategoryPage.vue'
+// import CollectionPage from '@/views/admin/collection/CollectionPage.vue'
+// import SystemConfigPage from '@/views/admin/system/SystemConfigPage.vue'
+// import StylePage from '@/views/admin/style/StylePage.vue'
+// import OrderPage from '@/views/order/OrderPage.vue'
+// import ProductLinePage from '@/views/admin/product/ProductLinePage.vue'
+// import ProductPage from '@/views/admin/product/ProductPage.vue'
+// import UserPage from '@/views/admin/user/UserPage.vue'
+// import BrandPage from '@/views/admin/brand/BrandPage.vue'
+// import ProductVariantPage from '@/views/admin/product/ProductVariantPage.vue'
+// import ProductTypePage from '@/views/admin/product-type/ProductTypePage.vue'
+// import SystemNotFoundPage from '@/views/admin/system/NotFoundPage.vue'
 
 //Detail
 import ProductVariantItemPage from '@/views/admin/product/ProductVariantItemPage.vue'
@@ -29,12 +29,14 @@ import BrandDetail from '@/views/admin/brand/BrandDetail.vue'
 import ProductLineDetail from '@/views/admin/product/ProductLineDetail.vue'
 import StyleDetail from '@/views/admin/style/StyleDetail.vue'
 import ProductTypeDetail from '@/views/admin/product-type/ProductTypeDetail.vue'
+import ProductDetail from '@/views/admin/product/ProductDetail.vue'
+import CollectionDetail from '@/views/admin/collection/CollectionDetail.vue'
+
 //notfound
 import NotFoundPage from '@/views/NotFoundPage.vue'
 
 //
 import { useAuthStore } from '@/stores/auth'
-import CollectionDetail from '@/views/admin/collection/CollectionDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -75,130 +77,158 @@ const router = createRouter({
         {
           path: 'dashboard',
           name: ROUTE_NAMES.DASHBOARD,
-          component: () => DashBoardPage,
+          component: () => import('@/views/admin/DashBoardPage.vue'),
         },
         {
           path: 'profile',
           name: ROUTE_NAMES.PROFILE,
-          component: () => DashBoardPage,
+          component: () => import('@/views/admin/DashBoardPage.vue'),
         },
 
         {
           path: 'categories',
           name: ROUTE_NAMES.CATEGORIES,
-          component: () => CategoryPage,
+          component: () => import('@/views/admin/category/CategoryPage.vue'),
         },
 
         {
           path: 'users',
           name: ROUTE_NAMES.USERS,
-          component: () => UserPage,
+          component: () => import('@/views/admin/user/UserPage.vue'),
         },
 
         {
           path: 'brands',
           name: ROUTE_NAMES.BRANDS,
-          component: () => BrandPage,
+          component: () => import('@/views/admin/brand/BrandPage.vue'),
         },
 
         {
           path: 'products',
           name: ROUTE_NAMES.PRODUCTS,
-          component: () => ProductPage,
+          component: () => import('@/views/admin/product/ProductPage.vue'),
         },
 
         {
           path: 'styles',
           name: ROUTE_NAMES.STYLES,
-          component: () => StylePage,
+          component: () => import('@/views/admin/style/StylePage.vue'),
         },
         {
           path: 'product-types',
           name: ROUTE_NAMES.PRODUCT_TYPES,
-          component: () => ProductTypePage,
+          component: () => import('@/views/admin/product-type/ProductTypePage.vue'),
         },
 
         {
           path: 'system-config',
           name: ROUTE_NAMES.SYSTEM_CONFIG,
-          component: () => SystemConfigPage,
+          component: () => import('@/views/admin/system/SystemConfigPage.vue'),
         },
 
         {
           path: 'collections',
           name: ROUTE_NAMES.COLLECTIONS,
-          component: () => CollectionPage,
+          component: () => import('@/views/admin/collection/CollectionPage.vue'),
         },
 
         {
           path: 'product-lines',
           name: ROUTE_NAMES.PRODUCT_LINES,
-          component: () => ProductLinePage,
+          component: () => import('@/views/admin/product/ProductLinePage.vue'),
         },
 
         {
           path: 'orders',
           name: ROUTE_NAMES.ORDERS,
-          component: () => OrderPage,
+          component: () => import('@/views/order/OrderPage.vue'),
         },
 
         {
           path: 'product-variants',
           name: ROUTE_NAMES.PRODUCT_VARIANTS,
-          component: () => ProductVariantPage,
+          component: () => import('@/views/admin/product/ProductVariantPage.vue'),
         },
 
         {
           path: 'product-variant-items',
           name: ROUTE_NAMES.PRODUCT_VARIANT_ITEMS,
-          component: () => ProductVariantItemPage,
+          component: () => import('@/views/admin/product/ProductVariantItemPage.vue'),
         },
 
         // Not Found
         {
           path: '404',
           name: ROUTE_NAMES.NOT_FOUND,
-          component: SystemNotFoundPage,
+          component: import('@/views/admin/system/NotFoundPage.vue'),
         },
         {
           path: ':pathMatch(.*)*',
           component: () => NotFoundPage,
         },
+
+        // detail
+        {
+          path: 'categories/:slug',
+          name: ROUTE_NAMES.CATEGORY_DETAIL,
+          component: () => CategoryDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.CATEGORIES,
+          },
+        },
+
+        {
+          path: 'brands/:slug',
+          name: ROUTE_NAMES.BRAND_DETAIL,
+          component: () => BrandDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.BRANDS,
+          },
+        },
+
+        {
+          path: 'product-lines/:slug',
+          name: ROUTE_NAMES.PRODUCT_LINE_DETAIL,
+          component: () => ProductLineDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.PRODUCT_LINES,
+          },
+        },
+
+        {
+          path: 'styles/:slug',
+          name: ROUTE_NAMES.STYLE_DETAIL,
+          component: () => StyleDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.STYLES,
+          },
+        },
+
+        {
+          path: 'collections/:slug',
+          name: ROUTE_NAMES.COLLECTION_DETAIL,
+          component: () => CollectionDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.COLLECTIONS,
+          },
+        },
+        {
+          path: 'product-types/:slug',
+          name: ROUTE_NAMES.PRODUCT_TYPE_DEAIL,
+          component: () => ProductTypeDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.PRODUCT_TYPES,
+          },
+        },
+        {
+          path: 'products/:slug',
+          name: ROUTE_NAMES.PRODUCT_DETAIL,
+          component: () => ProductDetail,
+          meta: {
+            activeMenu: ROUTE_NAMES.PRODUCTS,
+          },
+        },
       ],
-    },
-    {
-      path: '/admin/categories/:slug',
-      name: ROUTE_NAMES.CATEGORY_DETAIL,
-      component: () => CategoryDetail,
-    },
-
-    {
-      path: '/admin/brands/:slug',
-      name: ROUTE_NAMES.BRAND_DETAIL,
-      component: () => BrandDetail,
-    },
-
-    {
-      path: '/admin/product-lines/:slug',
-      name: ROUTE_NAMES.PRODUCT_LINE_DETAIL,
-      component: () => ProductLineDetail,
-    },
-
-    {
-      path: '/admin/styles/:slug',
-      name: ROUTE_NAMES.STYLE_DETAIL,
-      component: () => StyleDetail,
-    },
-
-    {
-      path: '/admin/collestions/:slug',
-      name: ROUTE_NAMES.COLLECTION_DETAIL,
-      component: () => CollectionDetail,
-    },
-    {
-      path: '/admin/product-types/:slug',
-      name: ROUTE_NAMES.PRODUCT_TYPE_DEAIL,
-      component: () => ProductTypeDetail,
     },
   ],
 })
